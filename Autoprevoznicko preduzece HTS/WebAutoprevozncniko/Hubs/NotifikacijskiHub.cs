32ffe08a39f2b2059ec3f23596b3b5a26739ccd4
Almin Hatari? <almin.hataric@edu.fit.ba>
2021-02-10 09:38:49 +0100

    .

---
+++ Autoprevoznicko preduzece HTS/WebAutoprevozncniko/Hubs/NotifikacijskiHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAutoprevozncniko.Hubs
+{
+    public class NotifikacijskiHub : Hub
+    {
+        public static void Show()
+        {
+        }
+    }
+}
